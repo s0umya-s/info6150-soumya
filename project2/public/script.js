1714f8ff3dc79c7dcee09b02b9f0f18c7dd1bba3
Soumya Singh <singh.soumy@northeastern.edu>
2025-03-13 19:55:12 -0400

    adding more button functionalities for subscribe and okay dialogues

--- project2/public/script.js
+++ project2/public/script.js
@@ -1,22 +1,35 @@
 "use strict";
 
+
 document.querySelector(".hamburger").addEventListener("click", function () {
     document.querySelector(".nav-links").classList.toggle("active");
 });
 
 
 const modal = document.getElementById("subscribe-modal");
+const successModal = document.getElementById("success-modal");
+
 document.querySelectorAll(".subscribe-btn").forEach(button => {
     button.addEventListener("click", () => modal.showModal());
 });
-document.querySelector(".close-modal").addEventListener("click", () => modal.close());
 
-document.querySelector("form").addEventListener("submit", function (e) {
+document.querySelectorAll(".close-modal").forEach(button => {
+    button.addEventListener("click", () => {
+        modal.close();
+        successModal.close();
+    });
+});
+
+document.querySelector("#subscribe-modal form").addEventListener("submit", function (e) {
     const email = document.getElementById("email").value;
     const confirmEmail = document.getElementById("confirm-email").value;
 
     if (!email.includes("@") || email !== confirmEmail) {
         alert("Emails must match and contain '@'.");
-        e.preventDefault();
+        e.preventDefault(); 
+    } else {
+        e.preventDefault(); 
+        modal.close();
+        successModal.showModal(); 
     }
 });
